e6459165599995d47e68d416223cf5c30bea75f9
aiperchick <aiperitif.03@gmail.com>
2023-02-14 23:44:39 +0600

    hw1

---
+++ blog/views.py
@@ -0,0 +1,14 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+from blog import models
+
+
+def hello_world(request):
+    return HttpResponse('Hello World')
+
+
+def blog_all(request):
+    post = models.Post.objects.all()
+    return render(request, 'post_list.html', {'post': post})
+
