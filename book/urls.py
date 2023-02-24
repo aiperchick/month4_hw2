7bf5d1357e3019802c83f7c9e431416a6712b03b
aiperchick <aiperitif.03@gmail.com>
2023-02-24 21:54:26 +0600

    hw4

--- book/urls.py
+++ book/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('books/', views.books, name='books'),
     path('details/<int:id>/', views.book_full_view, name='details'),
+    path('books/<int:id>/update/', views.update_book_show, name='update'),
+    path('books/<int:id>/delete/', views.delete_book_show, name='delete'),
+    path('add_book/', views.book_show_form, name='create'),
 ]
--- book/views.py
+++ book/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_list_or_404
-from book import models
+from django.http import HttpResponse
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from book import models, forms
 
 
 def books(requst):
@@ -10,3 +11,43 @@ def books(requst):
 def book_full_view(request, id):
     book_id = get_list_or_404(models.Book, id=id)
     return render(request, 'book.full.html', {'books_id': book_id})
+
+
+# Добавление книг через формы
+def book_show_form(request):
+    method = request.method
+    if method == 'POST':
+        form = forms.BookForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>книга добавлена</h2>')
+
+    else:
+        form = forms.BookForm()
+
+    return render(request, 'add_bookshow.html', {'form': form})
+
+
+# изменение данных о книге
+def update_book_show(request, id):
+    show_object = get_object_or_404(models.Book, id=id)
+    if request.method == 'POST':
+        form = forms.BookForm(instance=show_object, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('<h2>Книга успешно обновлена!</h2>')
+    else:
+        form = forms.BookForm(instance=show_object)
+
+    return render(request, 'update_bookshow.html', {
+                                                    'form': form,
+                                                    'object': show_object
+                                                   })
+
+
+# Удаление из базы
+def delete_book_show(request, id):
+    show_object = get_object_or_404(models.Book, id=id)
+    show_object.delete()
+    return HttpResponse('<h2>Книга удалена</h2>')
+
